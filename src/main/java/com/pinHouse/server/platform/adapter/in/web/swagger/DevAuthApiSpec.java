5f61057753f137fe73f1ad82f2afc1908a3ce2a7
eedo_y <doup2001@naver.com>
2025-08-12 00:06:20 +0900

    ✨ feat : 개발용 토큰 발급 기능 구현

---
+++ src/main/java/com/pinHouse/server/platform/adapter/in/web/swagger/DevAuthApiSpec.java
@@ -0,0 +1,17 @@
+package com.pinHouse.server.platform.adapter.in.web.swagger;
+
+import com.pinHouse.server.core.response.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Tag(name = "개발용 JWT 발급 API", description = "개발용 JWT 발급 API입니다.")
+public interface DevAuthApiSpec {
+
+    @Operation(
+            summary = "쿠키 발급 API",
+            description = "개발의 편의를 위해 개발용 JWT를 즉시 발급 받을 수 있습니다."
+    )
+    ApiResponse<Void> devLogin(HttpServletResponse response);
+
+}
