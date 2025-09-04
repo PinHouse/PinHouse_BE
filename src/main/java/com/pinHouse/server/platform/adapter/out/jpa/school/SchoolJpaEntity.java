3a7724d1db4780657ce3be6d5d744350f7f1a7b7
eedo_y <doup2001@naver.com>
2025-09-04 13:33:35 +0900

    ✨ feat : 학교 도메인 정의

--- src/main/java/com/pinHouse/server/platform/adapter/out/jpa/school/SchoolJpaEntity.java
+++ src/main/java/com/pinHouse/server/platform/adapter/out/jpa/school/SchoolJpaEntity.java
@@ -1,5 +1,6 @@
 package com.pinHouse.server.platform.adapter.out.jpa.school;
 
+import com.pinHouse.server.platform.domain.school.School;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,10 +16,46 @@ import lombok.NoArgsConstructor;
 public class SchoolJpaEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
+
+    private String schoolName;
+
+    private String schoolType;
+
+    private String schoolCategory;
+
+    private String sidoCode;
+
+    private String sidoName;
+
+    private String sigunguName;
+
+    /// 정적 팩토리 메서드
+    public static SchoolJpaEntity from(School school) {
+        return SchoolJpaEntity.builder()
+                .id(school.getId())
+                .schoolName(school.getSchoolName())
+                .schoolType(school.getSchoolType())
+                .schoolCategory(school.getSchoolCategory())
+                .sidoCode(school.getSidoCode())
+                .sidoName(school.getSidoName())
+                .sigunguName(school.getSigunguName())
+                .build();
+    }
 
 
+    /// 도메인 변환 메서드
+    public School toDomain() {
+        return School.builder()
+                .id(getId())
+                .schoolName(getSchoolName())
+                .schoolType(getSchoolType())
+                .schoolCategory(getSchoolCategory())
+                .sidoCode(getSidoCode())
+                .sidoName(getSidoName())
+                .sigunguName(getSigunguName())
+                .build();
+    }
 
 
 
