a259732040f118f712cd577d64036cc08ee16d92
eedo_y <doup2001@naver.com>
2025-09-25 21:56:10 +0900

    ✨feat: 학교 탐색 관련 기능 구현

--- src/main/java/com/pinHouse/server/platform/housingFit/school/application/usecase/SchoolUseCase.java
+++ src/main/java/com/pinHouse/server/platform/housingFit/school/application/usecase/SchoolUseCase.java
@@ -1,7 +1,21 @@
 package com.pinHouse.server.platform.housingFit.school.application.usecase;
 
+import com.pinHouse.server.platform.housingFit.school.application.dto.response.SchoolResponse;
+
+import java.util.List;
+
 public interface SchoolUseCase {
 
+    /// 고등학교 검색 로직
+    List<SchoolResponse> searchSchool(String schoolName);
+
+    /// 고등학교 가능한건지
+    String existSchool(String schoolName);
+
     /// 대학교 검색 로직
+    List<SchoolResponse> searchUniversity(String universityName);
+
+    /// 대학교 가능한건지
+    String existUniversity(String schoolName);
 
 }
