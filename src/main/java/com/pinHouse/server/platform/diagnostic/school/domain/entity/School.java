e0e9ced2d27cce9862f2b01c49f6f64e8290a879
eedo_y <doup2001@naver.com>
2025-10-12 11:38:34 +0900

    ♻️refactor : 학교 도메인 수정

--- src/main/java/com/pinHouse/server/platform/diagnostic/school/domain/entity/School.java
+++ src/main/java/com/pinHouse/server/platform/diagnostic/school/domain/entity/School.java
@@ -1,31 +1,49 @@
 package com.pinHouse.server.platform.diagnostic.school.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "school")
+import java.util.UUID;
+
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @Getter
+@Table(name = "school")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class School {
 
     @Id
     private String id;
 
+    @Column(nullable = false)
     private String schoolName;
 
+    @Column(nullable = false)
     private String schoolType;
 
+    @Column(nullable = false)
     private String schoolCategory;
 
+    @Column(nullable = false)
     private String sidoCode;
 
+    @Column(nullable = false)
     private String sidoName;
 
+    @Column(nullable = false)
     private String sigunguName;
+
+    /// 빌더 생성자
+    @Builder
+    public School(String schoolName, String schoolType, String schoolCategory, String sidoCode, String sidoName, String sigunguName) {
+        this.id = UUID.randomUUID().toString();
+        this.schoolName = schoolName;
+        this.schoolType = schoolType;
+        this.schoolCategory = schoolCategory;
+        this.sidoCode = sidoCode;
+        this.sidoName = sidoName;
+        this.sigunguName = sigunguName;
+    }
 }
