bf6cd6f7e19d486cd153147f3473e101b8656bac
eedo_y <doup2001@naver.com>
2025-10-12 11:22:52 +0900

    💡style : 개발용 토큰 발급 스웨거 수정

--- src/main/java/com/pinHouse/server/security/auth/presentation/swagger/DevAuthApiSpec.java
+++ src/main/java/com/pinHouse/server/security/auth/presentation/swagger/DevAuthApiSpec.java
@@ -11,7 +11,7 @@ public interface DevAuthApiSpec {
     /// 재발급
     @Operation(
             summary = "개발용 토큰 발급 API",
-            description = "개발의 편의를 위해 개발용 JWT를 즉시 발급 받을 수 있습니다."
+            description = "개발용 액세스 토큰(1시간) 및 리프레쉬 토큰을 즉시 발급 받을 수 있습니다."
     )
     ApiResponse<Void> devLogin(HttpServletResponse response);
 
