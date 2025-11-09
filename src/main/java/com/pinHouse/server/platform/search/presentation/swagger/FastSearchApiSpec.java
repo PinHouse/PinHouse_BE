fc1ffc002f2599b2714bdb52a20f745cc8fcf3b6
eedo_y <doup2001@naver.com>
2025-11-09 17:51:42 +0900

    💡style: 검색 API 스펙 추가

--- src/main/java/com/pinHouse/server/platform/search/presentation/swagger/FastSearchApiSpec.java
+++ src/main/java/com/pinHouse/server/platform/search/presentation/swagger/FastSearchApiSpec.java
@@ -3,6 +3,7 @@ package com.pinHouse.server.platform.search.presentation.swagger;
 import com.pinHouse.server.core.response.response.ApiResponse;
 import com.pinHouse.server.platform.search.application.dto.FastSearchRequest;
 import com.pinHouse.server.platform.search.application.dto.FastSearchResponse;
+import com.pinHouse.server.platform.search.application.dto.SearchHistoryResponse;
 import com.pinHouse.server.security.oauth2.domain.PrincipalDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,5 +20,12 @@ public interface FastSearchApiSpec {
     ApiResponse<FastSearchResponse> search(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @RequestBody FastSearchRequest request);
+
+
+    @Operation(
+            summary = "빠른 검색 존재여부 API",
+            description = "제일 최근 빠른 검색 존재여부 API 입니다."
+    )
+    ApiResponse<SearchHistoryResponse> history(@AuthenticationPrincipal PrincipalDetails principalDetails);
 
 }
