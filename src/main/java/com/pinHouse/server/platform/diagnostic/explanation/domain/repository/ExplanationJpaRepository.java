c98318a6312076dbb4cdb4b721ce2ff6824cd48b
eedo_y <doup2001@naver.com>
2025-12-07 19:04:29 +0900

    ♻️refactor : 필요없는 설명 API 제거

--- src/main/java/com/pinHouse/server/platform/diagnostic/explanation/domain/repository/ExplanationJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pinHouse.server.platform.diagnostic.explanation.domain.repository;
-
-import com.pinHouse.server.platform.diagnostic.explanation.domain.entity.Explanation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExplanationJpaRepository extends JpaRepository<Explanation, Long> {
-}
