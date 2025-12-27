4f1ba6951e6f8a0c67120e565a20a89909fd8f3c
eedo_y <doup2001@naver.com>
2025-12-28 02:11:39 +0900

    ✨feat : 방 그룹 관련 기능 구현

--- src/main/java/com/pinHouse/server/platform/housing/complex/application/dto/response/UnitTypeResponse.java
+++ src/main/java/com/pinHouse/server/platform/housing/complex/application/dto/response/UnitTypeResponse.java
@@ -4,6 +4,8 @@ import com.pinHouse.server.platform.housing.complex.domain.entity.Quota;
 import com.pinHouse.server.platform.housing.complex.domain.entity.UnitType;
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 public record UnitTypeResponse(
         String typeId,
@@ -12,7 +14,8 @@ public record UnitTypeResponse(
         Integer quota,          // 모집호수 정보
         Double exclusiveAreaM2, // 전용면적
         DepositResponse deposit,    /// 최대/보통/최소
-        boolean liked               ///  좋아요
+        boolean liked,               ///  좋아요
+        List<String> group           /// 공급 그룹 정보
 ) {
 
     /// 정적 팩토리 메서드
@@ -28,6 +31,7 @@ public record UnitTypeResponse(
                 .deposit(deposit)
                 .quota(typeQuota.getTotal())
                 .liked(liked)
+                .group(unitType.getGroup())
                 .build();
 
     }
