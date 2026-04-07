6503f76c1fd55af4f7813c0e9a8a4884c924d3c5
eedo_y <doup2001@naver.com>
2026-04-07 16:56:43 +0900

    refactor: 보안 모듈 추가 (#98)

--- module-infrastructure/src/main/java/co/kr/pinhouse/common/config/swagger/LocalSwaggerConfig.java
+++ module-infrastructure/src/main/java/co/kr/pinhouse/common/config/swagger/LocalSwaggerConfig.java
@@ -1,4 +1,4 @@
-package co.kr.pinhouse.api.config.swagger;
+package co.kr.pinhouse.common.config.swagger;
 
 import java.util.Map;
 import java.util.TreeMap;
@@ -8,22 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Schema;
 
-/**
- * Swagger / OpenAPI 설정 클래스
- * - HTTPS 설정으로 인해 @OpenAPIDefinition 추가
- * - 스웨거 내부 JWT 설정 추가
- */
-
+@Profile("local")
 @Configuration
-@Profile("prod")
-@OpenAPIDefinition(servers = {@Server(url = "https://api.pinhouse.co.kr", description = "PinHouse 운영 서버")})
-public class ProdSwaggerConfig {
+public class LocalSwaggerConfig {
 
 	@Bean
 	public OpenAPI openApi() {
@@ -34,7 +25,7 @@ public class ProdSwaggerConfig {
 	private Info apiInfo() {
 		return new Info()
 			.title("PinHouse Swagger")
-			.description("핀하우스 스웨거입니다.")
+			.description("핀하우스 로컬 스웨거입니다.")
 			.version("1.0.0");
 	}
 
