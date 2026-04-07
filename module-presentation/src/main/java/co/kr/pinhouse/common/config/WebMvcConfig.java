83d69049ef77535e51c3d487cbfc789f5006da21
eedo_y <doup2001@naver.com>
2026-04-07 17:28:17 +0900

    refactor: 스웨거 Config 위치 이동 (#98)

---
+++ module-presentation/src/main/java/co/kr/pinhouse/common/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package co.kr.pinhouse.common.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import co.kr.pinhouse.common.auth.CurrentUserIdArgumentResolver;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final CurrentUserIdArgumentResolver currentUserIdArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(currentUserIdArgumentResolver);
+	}
+}
